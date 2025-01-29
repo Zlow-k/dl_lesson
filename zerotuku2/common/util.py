09ec19cf3a50601e7d144f16ce8648cb6c0e7f5f
Zlow-k <yj.kasai@gmail.com>
2025-01-29 09:58:51 +0900

    zerotuku2

---
+++ zerotuku2/common/util.py
@@ -0,0 +1,19 @@
+import numpy as np
+import re
+
+def preprocess(text):
+    text = text.lower()
+    words = re.findall(r"\w+|[.,!?]", text)
+    
+    word_to_id = {}
+    id_to_word = {}
+
+    for word in words:
+        if word not in word_to_id:
+            new_id = len(word_to_id)
+            word_to_id[word] = new_id
+            id_to_word[new_id] = word
+            
+    corpus = np.array([word_to_id[w] for w in words])
+    
+    return corpus, word_to_id, id_to_word
